388016dbadb8403a13ba376c20ac31b93910c1eb
Peter Lithammer <peter.lithammer@gmail.com>
2020-01-28 14:06:52 +0100

    Make journal name completion exclusive

--- completions/journal.fish
+++ completions/journal.fish
@@ -1,4 +1,4 @@
+complete -c journal -x -a '(journal -l)'
 complete -c journal -s h -d 'Display help and exit'
 complete -c journal -s l -d 'List journals'
 complete -c journal -s r -x -a '(journal -l)' -d 'Remove journal'
-complete -c journal -f -a '(journal -l)'
