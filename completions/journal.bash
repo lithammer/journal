5dd2b5e3c8517014da6c4fa91fca2ab9ab570ade
Peter Lithammer <peter.lithammer@gmail.com>
2022-03-02 10:27:39 +0100

    Format bash/zsh code using `shfmt`

--- completions/journal.bash
+++ completions/journal.bash
@@ -1,21 +1,23 @@
+# shellcheck shell=bash
+
 _journal() {
-    local cur prev opts
+	local cur prev opts
 
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h -l -r"
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD - 1]}"
+	opts="-h -l -r"
 
-    if [[ $cur == -* ]]; then
-        mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
-        return 0
-    fi
+	if [[ $cur == -* ]]; then
+		mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
+		return 0
+	fi
 
-    case "$prev" in
-        -h | -l) return 0 ;;
-        *) ;;
-    esac
+	case "$prev" in
+	-h | -l) return 0 ;;
+	*) ;;
+	esac
 
-    mapfile -t COMPREPLY < <(compgen -W "$(journal -l)" -- "$cur")
+	mapfile -t COMPREPLY < <(compgen -W "$(journal -l)" -- "$cur")
 }
 
 complete -F _journal journal
